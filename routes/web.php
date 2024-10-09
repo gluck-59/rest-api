3804878f0ce16b83bbb3a72b2234a42618e9e11d
gluck <gluck59@gmail.com>
2024-10-09 13:47:58 +0500

    []

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return false;
 });
+
+
+
+Route::get('/items', [TodoController::class, 'index']);
+Route::get('/items/{id}', [TodoController::class, 'show']);
+Route::post('/items', [TodoController::class, 'store']);
+Route::put('/items/{id}', [TodoController::class, 'update']);
+Route::delete('/items/{id}', [TodoController::class, 'destroy']);
+
